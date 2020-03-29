4cfc9d326f53510b03d76cd11021dbc9e89f61e3
Lynda Metref <lynda.metref@gmail.com>
2020-03-29 17:30:48 +0200

    Correct message content

--- plugins/command/announce.py
+++ plugins/command/announce.py
@@ -1,1 +1,40 @@
-../../../nikola-announce/announce.py
+from nikola.plugin_categories import Command
+from nikola.post import get_meta
+from datetime import datetime
+from discord_webhook import DiscordWebhook, DiscordEmbed
+
+
+# You have to inherit Command for this to be a
+# command plugin:
+
+class CommandServe(Command):
+    """Announce today's post on various services using webhooks"""
+
+    name = "announce"
+    doc_usage = "[options]"
+    doc_purpose = "announce today's post on various services using webhooks"
+
+    cmd_options = (
+        {
+            'name': 'discord-webhook-url',
+            'short': 'u',
+            'long': 'discord-webhook-url',
+            'type': str,
+            'default': '',
+            'help': 'Webhood of discord channel',
+        },
+    )
+
+    def _execute(self, options, args):
+        """Announce today's post on various services using webhooks"""
+        self.site.scan_posts()
+        today_post = [x for x in self.site.timeline if x.date.date() == datetime.today().date()]
+        meta = get_meta(today_post[0], 'fr')
+        content = list([today_post[0].title()])
+        content.append(today_post[0].description())
+        [content.append(x) for x in meta[0]['references']]
+        content_str = "\n".join(content)
+
+        webhook = DiscordWebhook(url=options['discord-webhook-url'], content=content_str)
+        response = webhook.execute()
+        return response
--- posts/2020/03/tides-of-history-jakob-fugger-the-richest-man-who-ever-lived.md
+++ posts/2020/03/tides-of-history-jakob-fugger-the-richest-man-who-ever-lived.md
@@ -1,11 +1,10 @@
 ---
-category: ''
 date: 2020-03-29 17:00:00 UTC+02:00
-description: ''
-link: ''
+description: "Qu'est ce que ça veut dire être riche? Et super mega extra riche? Est-ce que ça vaudrait vraiment la peine de vivre au 16ème pour l'être?"
 slug: tides-of-history-jakob-fugger-the-richest-man-who-ever-lived
-tags: ''
 title: 'Tides of History - Jakob Fugger: The Richest Man Who Ever Lived?'
+references: 
+    - https://art19.com/shows/tides-of-history/episodes/ce7ec21d-66d5-4a0b-adfc-2ab2dcabcc4d
 type: text
 ---
 
