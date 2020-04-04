cab618021bd91ae12050524be9c99f5eff22f0ff
Lynda Metref <lynda.metref@gmail.com>
2020-04-04 18:42:09 +0200

    Color indications in list of future posts

--- plugins/command/list-future-posts.py
+++ plugins/command/list-future-posts.py
@@ -1,5 +1,5 @@
 from nikola.plugin_categories import Command
-from datetime import datetime
+from datetime import datetime, timedelta
 from nikola.post import get_meta
 
 
@@ -21,10 +21,21 @@ class CommandServe(Command):
             meta = get_meta(post, 'fr')
             has_reference = len(meta[0]['references']) > 0
             has_description = len(post.description()) > 0
-            return has_reference & has_description
+            if has_reference & has_description:
+                return '\033[92m Valid \033[0m'
+            else:
+                return '\033[91m Invalid \033[0m'
+
+        def color_today_tomorrow(date):
+            if date.date() == datetime.today().date():
+                return '\033[92m' + str(date.date()) + '\033[0m'
+            elif date.date() == (datetime.today().date() + timedelta(days=1)):
+                return '\033[94m' + str(date.date()) + '\033[0m'
+            else:
+                return str(date.date())
 
         [print(validate(x), " ",
-               x.date,
+               color_today_tomorrow(x.date),
                " http://127.0.0.1:8000", x.permalink(), sep='')
          for x in self.site.timeline
          if x.date.date() >= datetime.today().date()]
